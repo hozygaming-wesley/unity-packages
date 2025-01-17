931f6e665fb8fd422191a091085a3225eb4b7a1f
hozygaming-wesley <cips@greenwingsgames.com>
2025-01-17 17:21:54 +0800

    chore:先告一段落

---
+++ Assets/Scripts/RenameTool.cs
@@ -0,0 +1,255 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wesley.Tool
+{
+    public class RenameTool : EditorWindow
+    {
+        #region members
+        private Vector2 m_scrollPos;
+        private string m_styleName = string.Empty;
+        private string m_splitSymbol = string.Empty;
+        private string m_insertFrontName = string.Empty;
+        private string m_originalName = string.Empty;
+        private string m_SecondName = string.Empty;
+        private string m_insertBackName = string.Empty;
+        private string m_previewName = string.Empty;
+        private string m_startNum_str = "0";
+        private int m_startNum_int = 0;
+        #endregion
+        #region get window
+        [MenuItem("Wesley/Rename Tool")]
+        private static void OpenWindow()
+        {
+            GetWindow<RenameTool>("Rename Tool").Show();
+        }
+        #endregion
+        #region render the window
+        private void OnGUI()
+        {
+            m_scrollPos = GUILayout.BeginScrollView(m_scrollPos);
+            {
+
+                GUILayout.BeginVertical();
+                {
+                    #region text input
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Split Symbol : ");
+                        m_splitSymbol = GUILayout.TextField(m_splitSymbol, GUILayout.Width(30));
+
+                        GUILayout.Label("Style Name : ");
+                        m_styleName = GUILayout.TextField(m_styleName, GUILayout.Width(70));
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Front Name: ");
+                        m_insertFrontName = GUILayout.TextField(m_insertFrontName, GUILayout.Width(70));
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Original Name: ");
+                        m_originalName = GUILayout.TextField(m_originalName, GUILayout.Width(70));
+                    }
+                    GUILayout.EndHorizontal();
+
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Back Name: ");
+                        m_insertBackName = GUILayout.TextField(m_insertBackName, GUILayout.Width(70));
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Second Name: ");
+                        m_SecondName = GUILayout.TextField(m_SecondName, GUILayout.Width(70));
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Start Number: ");
+                        m_startNum_str = GUILayout.TextField(m_startNum_str, GUILayout.Width(60));
+                        try
+                        {
+                            m_startNum_int = int.Parse(m_startNum_str);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+
+
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Preview:");
+                        GUILayout.Label(m_previewName);
+
+                    }
+                    GUILayout.EndHorizontal();
+
+
+
+
+                    #endregion
+                    #region button
+                    bool hasObject = (Selection.objects.Length > 0);
+                    GUI.enabled = hasObject;
+
+                    GUILayout.FlexibleSpace();
+                    if (!hasObject)
+                    {
+                        GUI.color = Color.red;
+                        GUILayout.Button("No Selected Objects!");
+                        GUI.color = Color.white;
+
+                    }
+                    else
+                    {
+                    }
+                    if (GUILayout.Button("Preview"))
+                    {
+                        Preview();
+                    }
+                    if (GUILayout.Button("Rename"))
+                    {
+                        Rename();
+
+                    }
+                    #endregion
+                }
+                GUILayout.EndVertical();
+            }
+            GUILayout.EndScrollView();
+        }
+        #endregion
+        #region Rename Function
+        void Preview()
+        {
+            if (!docked) return;
+            string insertFront = m_insertFrontName.Trim();//去除头尾空白字符串
+            string insertBack = m_insertBackName.Trim();//去除头尾空白字符串
+            int index = m_startNum_int;
+
+            if ((name + index) != string.Empty)//若名字不为空
+            {
+
+                foreach (Object o in Selection.objects)
+                {
+                    bool doubleSplit = Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[1].Value != string.Empty | m_SecondName != string.Empty;
+                    string styleName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[1].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[1].Value;
+
+                    string originalName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[3].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[3].Value;
+                    string secondName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[5].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[5].Value;
+
+
+                    m_previewName = "";
+                    m_previewName += m_styleName != string.Empty ? m_styleName : styleName;
+                    m_previewName += m_splitSymbol != string.Empty ? m_splitSymbol : "";
+                    m_previewName += insertFront ?? "";
+                    m_previewName += m_originalName != string.Empty ? m_originalName : originalName;
+                    m_previewName += insertBack ?? "";
+                    m_previewName += doubleSplit ? m_splitSymbol : "";
+                    m_previewName += doubleSplit & m_SecondName != string.Empty ? m_SecondName : secondName;
+                    m_previewName += m_startNum_str != string.Empty ? index.ToString() : "";
+
+                    index++;
+                }
+
+            }
+        }
+        private void Rename()
+        {
+            string insertFront = m_insertFrontName.Trim();//去除头尾空白字符串
+            string insertBack = m_insertBackName.Trim();//去除头尾空白字符串
+
+
+            int index = m_startNum_int;
+            if ((name + index) != string.Empty)//若名字不为空
+            {
+                bool isAssetsObject = false;//flag, 是否是assets文件夹的资源
+
+                foreach (Object o in Selection.objects)
+                {
+                    bool doubleSplit = Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[1].Value != string.Empty | m_SecondName != string.Empty;
+                    string styleName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[1].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[1].Value;
+
+                    string originalName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[3].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[3].Value;
+                    string secondName = doubleSplit ?
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)({m_splitSymbol})(.*)").Groups[5].Value :
+                        Regex.Match(o.name, @$"(.*)({m_splitSymbol})(.*)").Groups[5].Value;
+
+                    string path_g = AssetDatabase.GetAssetPath(o);//获得选中物的路径
+                    //查看路径后缀
+                    if (Path.GetExtension(path_g) != "")//若后缀不为空, 则为assets文件夹物体
+                    {
+                        if (name.Length >= 2 && name.Substring(0, 2) == "m_")// m_ 开头会被吞
+                        {
+                            //用 M_ 修正
+                            string temp_name = name.Remove(0, 1);
+                            name = temp_name.Insert(0, "M");
+                        }
+
+                        o.name = "";
+                        o.name += m_styleName != string.Empty ? m_styleName : styleName;
+                        o.name += m_splitSymbol != string.Empty ? m_splitSymbol : "";
+                        o.name += insertFront ?? "";
+                        o.name += m_originalName != string.Empty ? m_originalName : originalName;
+                        o.name += insertBack ?? "";
+                        o.name += doubleSplit ? m_splitSymbol : "";
+                        o.name += doubleSplit & m_SecondName != string.Empty ? m_SecondName : secondName;
+                        o.name += m_startNum_str != string.Empty ? index.ToString() : "";
+                        AssetDatabase.RenameAsset(path_g, o.name);//改名API
+                        if (!isAssetsObject)
+                        {
+                            isAssetsObject = true;//修改flag
+                        }
+                    }
+                    else//后缀为空, 是场景物体
+                    {
+                        o.name = "";
+                        o.name += m_styleName != string.Empty ? m_styleName : styleName;
+                        o.name += m_splitSymbol != string.Empty ? m_splitSymbol : "";
+                        o.name += insertFront ?? "";
+                        o.name += m_originalName != string.Empty ? m_originalName : originalName;
+                        o.name += insertBack ?? "";
+                        o.name += doubleSplit ? m_splitSymbol : "";
+                        o.name += doubleSplit & m_SecondName != string.Empty ? m_SecondName : secondName;
+                        o.name += m_startNum_str != string.Empty ? index.ToString() : "";
+
+
+                    }
+                    index++;
+                    Undo.RegisterFullObjectHierarchyUndo(o, "Rename");
+                }
+                if (isAssetsObject)//若是assets文件夹资源, 则刷新assets
+                {
+                    AssetDatabase.SaveAssets();
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+        #endregion
+    }
+}
+#endif
---
+++ Assets/Scripts/UAlignTool.cs
@@ -0,0 +1,119 @@
+﻿#if UNITY_EDITOR
+
+
+
+using UnityEditor;
+using UnityEngine;
+namespace Wesley.Tool
+{
+    public class UAlignTool : EditorWindow
+    {
+        int width = 65;
+        int hight = 25;
+        [MenuItem("Wesley/AlignTool")]
+
+        private static void OpenWindow()
+        {
+            GetWindow<UAlignTool>().Show();
+        }
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("SetParentAlign", GUILayout.Width(100), GUILayout.Height(hight)))
+            {
+                SetParentAlign();
+            }
+            if (GUILayout.Button("AlignY", GUILayout.Width(width), GUILayout.Height(hight)))
+            {
+                Align(type.PosY);
+            }
+            if (GUILayout.Button("AlignX", GUILayout.Width(width), GUILayout.Height(hight)))
+            {
+                Align(type.PosX);
+            }
+            if (GUILayout.Button("AlignBoth", GUILayout.Width(75), GUILayout.Height(hight)))
+            {
+                Align(type.both);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginVertical();
+            if (Selection.activeGameObject | GUI.changed)
+            {
+                string alignGO = $"AlignObject \"{Selection.gameObjects[0].gameObject.name}\"" ?? "NoObject!!";
+                Selection.selectionChanged += () =>
+                {
+                    try { alignGO = $"AlignObject \"{Selection.gameObjects[0].gameObject.name}\""; } catch (System.Exception) { }
+                };
+                EditorGUILayout.LabelField(alignGO);
+                for (int i = 0; i < Selection.gameObjects.Length; i++)
+                {
+                    var display = $"{i} {Selection.gameObjects[i].gameObject.name} is seleted!" ?? "NoObject!!";
+                    EditorGUILayout.LabelField(display);
+                }
+            }
+
+            EditorGUILayout.EndVertical();
+
+            void SetParentAlign()
+            {
+                var alignRT = Selection.gameObjects[0]?.GetComponent<RectTransform>();
+
+
+                for (int i = 0; i < Selection.gameObjects.Length; i++)
+                {
+                    var GORT = Selection.gameObjects[i].GetComponent<RectTransform>();
+                    GameObjectUtility.SetParentAndAlign(Selection.gameObjects[i], alignRT.gameObject);
+                    Undo.SetTransformParent(GORT, alignRT, $"SetAlign");
+
+                }
+            }
+
+            void Align(type type)
+            {
+
+                var parentRT = Selection.gameObjects[0]?.GetComponent<RectTransform>();
+
+
+                switch (type)
+                {
+                    case type.both:
+                        for (int i = 1; i < Selection.gameObjects.Length; i++)
+                        {
+                            var childRT = Selection.gameObjects[i]?.GetComponent<RectTransform>();
+                            childRT.localPosition = new Vector3(parentRT.localPosition.x, parentRT.localPosition.y, parentRT.localPosition.z);
+                            Undo.RegisterFullObjectHierarchyUndo(Selection.gameObjects[i], "Align");
+                        }
+                        break;
+                    case type.PosX:
+                        for (int i = 1; i < Selection.gameObjects.Length; i++)
+                        {
+                            var childRT = Selection.gameObjects[i]?.GetComponent<RectTransform>();
+                            childRT.localPosition = new Vector3(parentRT.localPosition.x, childRT.localPosition.y, childRT.localPosition.z);
+                            Undo.RegisterFullObjectHierarchyUndo(Selection.gameObjects[i], "Align");
+                        }
+                        break;
+                    case type.PosY:
+                        for (int i = 1; i < Selection.gameObjects.Length; i++)
+                        {
+                            var childRT = Selection.gameObjects[i]?.GetComponent<RectTransform>();
+                            childRT.localPosition = new Vector3(childRT.localPosition.x, parentRT.localPosition.y, childRT.localPosition.z);
+                            Undo.RegisterFullObjectHierarchyUndo(Selection.gameObjects[i], "Align");
+                        }
+                        break;
+
+                }
+
+            }
+
+
+
+        }
+
+        public enum type
+        {
+            both, PosY, PosX
+        }
+    }
+}
+#endif
